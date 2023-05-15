e55462896be13086a11e3680bdc3140002ee68bc
yuya <nerentabelen@mail.ru>
2023-05-15 18:25:02 +0300

    minor text changes

--- myblog/news/views.py
+++ myblog/news/views.py
@@ -3,31 +3,32 @@ from .models import Articles
 from .forms import ArticlesForm
 from django.views.generic import DetailView, UpdateView, DeleteView, ListView
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.contrib.auth.decorators import login_required
+
 
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
-    paginator = Paginator(news, 4)  # 4 новости на странице
+    paginator = Paginator(news, 4)
     page = request.GET.get('page')
 
     try:
         news = paginator.page(page)
     except PageNotAnInteger:
-        # Если параметр страницы не является целым числом, отображаем первую страницу
         news = paginator.page(1)
     except EmptyPage:
-        # Если параметр страницы больше, чем общее количество страниц, отображаем последнюю страницу
         news = paginator.page(paginator.num_pages)
 
     return render(request, 'news/news_home.html', {'news': news})
 
-
+ # @login_required
 class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
 
 
+# @login_required
 class NewsUpdateView(UpdateView):
     model = Articles
     template_name = 'news/create.html'
@@ -35,26 +36,33 @@ class NewsUpdateView(UpdateView):
     form_class = ArticlesForm
 
 
+# @login_required
 class NewsDeleteView(DeleteView):
     model = Articles
     success_url = '/news/'
     template_name = 'news/news-delete.html'
 
 
+# @login_required
+
+@login_required
 def create(request):
     error = ''
     if request.method == 'POST':
         form = ArticlesForm(request.POST)
         if form.is_valid():
-            form.save()
+            article = form.save(commit=False)
+            article.user_id = request.user.id  # Установка user_id в текущего пользователя
+            article.save()
             return redirect('news_home')
         else:
             error = "Форма неверная"
 
     form = ArticlesForm()
 
-    date = {
+    data = {
         'form': form,
         'error': error
     }
-    return render(request, 'news/create.html', date)
+    return render(request, 'news/create.html', data)
+
